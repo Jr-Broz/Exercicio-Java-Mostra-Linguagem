9e3512acc752231eaa81f22f4cb241d8ab20577e
João Rafael Broz dos Santos <127759756+Jr-Broz@users.noreply.github.com>
2023-07-28 16:51:58 -0300

    uploading java files

---
+++ src/App.java
@@ -0,0 +1,17 @@
+import java.util.Locale;
+
+public class App {
+    public static void main(String[] args){ 
+        
+    Locale locale;
+
+    locale = Locale.getDefault();
+
+    System.out.println("A linguagem do sistema operacional está em:");
+
+    System.out.println(locale);
+
+
+    }
+}
+
